2846adac215e1e4aa9b19b19d34b67815483eedf
yohanF1245 <yohan.ferdin@hotmail.fr>
2024-11-21 16:32:15 +0100

    feat(populate-db-script.sql): add rooms insertion

--- populate-db-script.sql
+++ populate-db-script.sql
@@ -1,7 +1,18 @@
 INSERT INTO objets (id_objet, nom_objet, id_salle) VALUES
-(1, 'Poignard', 1),    -- Exemple : Salle 1
-(2, 'Revolver', 2),    -- Exemple : Salle 2
-(3, 'Corde', 3),       -- Exemple : Salle 3
-(4, 'Chandelier', 4),  -- Exemple : Salle 4
-(5, 'Clé anglaise', 5),-- Exemple : Salle 5
-(6, 'Tuyau de plomb', 6); -
+(1, 'Poignard', 1),    
+(2, 'Revolver', 2),    
+(3, 'Corde', 3),     
+(4, 'Chandelier', 4),  
+(5, 'Clé anglaise', 5),
+(6, 'Tuyau de plomb', 6); 
+
+INSERT INTO salles (nom_salle) VALUES
+('Salle de bal'),
+('Cuisine'),
+('Bibliothèque'),
+('Salon'),
+('Salle à manger'),
+('Hall'),
+('Jardin d\'hiver'),
+('Bureau'),
+('Salle de billard');
