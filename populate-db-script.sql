89677e6421c0aa3ffe4d7236a8913f758924551d
yohanF1245 <yohan.ferdin@hotmail.fr>
2024-11-21 16:27:57 +0100

    feat(populate-db-script.sql): add object insertion

---
+++ populate-db-script.sql
@@ -0,0 +1,7 @@
+INSERT INTO objets (id_objet, nom_objet, id_salle) VALUES
+(1, 'Poignard', 1),    -- Exemple : Salle 1
+(2, 'Revolver', 2),    -- Exemple : Salle 2
+(3, 'Corde', 3),       -- Exemple : Salle 3
+(4, 'Chandelier', 4),  -- Exemple : Salle 4
+(5, 'Clé anglaise', 5),-- Exemple : Salle 5
+(6, 'Tuyau de plomb', 6); -
