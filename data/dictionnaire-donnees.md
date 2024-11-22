b7afbd273081b12d7d0cd67b0a41fbbb9a244f25
Gabriel Luthun <gabriel.luthun@gmail.com>
2024-11-22 10:17:04 +0100

    docs(/): update repo structure

---
+++ data/dictionnaire-donnees.md
@@ -0,0 +1,22 @@
+## Dictionnaire de données
+
+Nom       | Type      | Description
+-------------- | --------- | ------------
+uuid_user      | UUID      | Identifiant unique de l'utilisateur
+pseudo         | text    | Pseudo de l'utilisateur
+role           | text    | Rôle de l'utilisateur (maître du jeu, détective ou observateur)
+id_personnage  | numeric      | Identifiant unique du personnage
+nom_perso    | text    | Nom du personnage
+couleur_perso | text    | Couleur du personnage
+image_perso | text    | Image du personnage
+description_perso | text    | Description du personnage
+id_salle | numeric | Identifiant unique de la salle
+nom_salle | text | Nom de la salle
+image_salle | text | Image de la salle
+description_salle | text | Description de la salle
+id_objet | numeric | Identifiant unique de l'objet
+nom_objet | text | Nom de l'objet
+description_objet | text | Description de l'objet
+image_objet | text | Image de l'objet
+heure_arrivee | time | Heure d'arrivée dans la salle
+heure_sortie | time | Heure de départ de la salle
---
+++ sql/create-db-script.sql
@@ -0,0 +1,42 @@
+DROP DATABASE simpluedo;
+CREATE USER simpluedo WITH ENCRYPTED PASSWORD 'trouvemoi';
+CREATE DATABASE simpluedo;
+
+\c simpluedo;
+
+CREATE TABLE roles (
+    id_roles VARCHAR(50) PRIMARY KEY,
+    nom_role VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE personnages (
+    id_perso INTEGER PRIMARY KEY,
+    nom_perso VARCHAR(50)
+);
+
+CREATE TABLE salles (
+    id_salle INTEGER PRIMARY KEY,
+    nom_salle VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE utilisateurs (
+    uuid_users UUID PRIMARY KEY,
+    pseudo_users VARCHAR(50),
+    id_roles VARCHAR(50) REFERENCES roles(id_roles),
+    id_perso INTEGER REFERENCES personnages(id_perso)
+);
+
+CREATE TABLE visiter (
+    heure_arrivee TIME,
+    heure_depart TIME,
+    id_perso INTEGER REFERENCES personnages(id_perso),
+    id_salle INTEGER REFERENCES salles(id_salle),
+    PRIMARY KEY (id_perso, id_salle, heure_arrivee)
+);
+CREATE TABLE objets (
+    id_objet INTEGER PRIMARY KEY,
+    nom_objet VARCHAR(50) UNIQUE,
+    id_salle INTEGER REFERENCES salles(id_salle)
+);
+
+GRANT all privileges ON DATABASE simpluedo to simpluedo;
